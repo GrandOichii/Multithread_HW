c0cb25853db970343d3c2872a6c0347efbd90f3b
oichii <i.hawk@yandex.ru>
2020-11-13 13:53:43 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <thread>
+#include <chrono>
+#include <cmath>
+
+using namespace std;
+using namespace std::chrono;
+
+string* result;
+
+vector<string> readStrings(const char* path) {
+    ifstream in(path);
+    vector<string> result(0);
+    string line;
+    while (in >> line)
+        result.push_back(line);
+    in.close();
+    return result;
+}
+
+string getCyph(char c, vector<string> keys) {
+    return keys[c - 'a'];
+}
+
+void encryptWord(string word, vector<string> keys, int i) {
+    string en = "";
+    for (int i = 0; i < word.size(); i++)
+        en += getCyph(word[i], keys);
+    result[i] = en;
+}
+
+void threadEncrypt(string text, int numOfThreads, vector<string> keys) {
+    auto startTime = high_resolution_clock::now();
+    
+    // main loop
+    result = new string[numOfThreads];
+    int wordLength = ceil(text.size() * 1.0 / numOfThreads);
+
+    string* words = new string[numOfThreads];
+    int begin = 0;
+    int i = 0;
+    while (1) {
+        // get the task
+        string line = text.substr(begin, wordLength);
+        thread t(encryptWord, line, keys, i);
+        t.join();
+        begin += wordLength;
+        ++i;
+        if (begin >= text.size()) { // no more tasks
+            break;
+        }
+    }
+
+    delete[] words;
+
+    cout << "Encrypted message: ";
+    for (int i = 0; i < numOfThreads; i++)
+        cout << result[i];
+    cout << endl;
+
+    auto stopTime = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(stopTime - startTime);
+    cout << "Time spent on parallel calculation (number of threads - " << numOfThreads << ") : " << duration.count() * 1.0 / 1000000 << endl << endl;
+}
+
+
+int main() {
+    auto keys = readStrings("key.txt");
+    auto strings = readStrings("text.txt");
+    string text = "";
+    for (string s : strings)
+        text += s;
+    cout << "Initial message: " << text << endl;
+    for (int i = 0; i < sqrt(text.size()); i++)
+        threadEncrypt(text, i + 1, keys);
+    cin.get();
+    delete[] result;
+    return 0;
+}
